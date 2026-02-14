4314c579fd5ee8eadec39e8510895f044b302ecc
Artem Churakov <119259462+kv4rtz@users.noreply.github.com>
2026-02-14 13:27:34 +0400

    Update main.py

--- t1/main.py
+++ t1/main.py
@@ -1,14 +1,14 @@
 class Calc:
-    def sum(self, a: float | int, b: float | int):
+    def sum(self, a: float, b: float):
         return a + b
     
-    def diff(self, a: float | int, b: float | int):
+    def diff(self, a: float, b: float):
         return a - b
     
-    def multiply(self, a: float | int, b: float | int):
+    def multiply(self, a: float, b: float):
         return a * b
     
-    def division(self, a: float | int, b: float | int):
+    def division(self, a: float, b: float):
         if (b == 0): raise ZeroDivisionError()
 
         return a / b
